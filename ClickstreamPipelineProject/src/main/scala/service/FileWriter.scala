63516a9a4302fe1a3e429e3734097804fcbea07b
Aradhya Singh <singharadhya2006.com>
2023-08-16 17:47:21 +0530

    running source code

---
+++ ClickstreamPipelineProject/src/main/scala/service/FileWriter.scala
@@ -0,0 +1,17 @@
+package service
+
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+
+object FileWriter {
+  def main(args: Array[String]) {
+    val spark = SparkSession.builder.master("local[*]").appName("FileWriter").getOrCreate()
+    //    val ssc = new StreamingContext(spark.sparkContext, Seconds(10))
+    val sc = spark.sparkContext
+    sc.setLogLevel("ERROR")
+    import spark.implicits._
+    import spark.sql
+
+    spark.stop()
+  }
+}
