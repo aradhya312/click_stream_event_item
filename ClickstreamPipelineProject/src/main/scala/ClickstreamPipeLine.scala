3ade328ce17e058868a0bc93fc3bbf323bb615df
Aradhya Singh <singharadhya2006.com>
2023-08-18 23:34:52 +0530

    Mysql Integration

--- ClickstreamPipelineProject/src/main/scala/ClickstreamPipeLine.scala
+++ ClickstreamPipelineProject/src/main/scala/ClickstreamPipeLine.scala
@@ -1,7 +1,12 @@
-import service.DataPipeline
+import database.DatabaseWrite
+import service.{DataPipeline, FileReader, FileWriter}
+
+import java.sql.Connection
+import java.sql.DriverManager
 
 object ClickstreamPipeLine {
   def main(args: Array[String]): Unit = {
     DataPipeline.dataPipeline()
+
   }
 }
